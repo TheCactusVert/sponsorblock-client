2091be7374a82c919eeeddef9d0095ae0b68785f
TheCactusVert <kevin@gavrois.fr>
2022-12-30 20:37:04 +0100

    refactor: changing most of the code

--- src/lib.rs
+++ src/lib.rs
@@ -4,33 +4,86 @@ mod segment;
 
 pub use action::Action;
 pub use category::Category;
+use segment::Videos;
 pub use segment::{Segment, Segments};
 
-use reqwest::{StatusCode, Url};
+use reqwest::{Client, Result, Url};
+use sha2::{Digest, Sha256};
+
+static USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0";
 
 pub async fn fetch<C, A>(
     server_address: Url,
-    privacy_api: bool,
     id: String,
     categories: C,
     action_types: A,
-) -> Option<Segments>
+) -> Result<Segments>
 where
     C: IntoIterator<Item = Category>,
     A: IntoIterator<Item = Action>,
 {
-    let segments = if privacy_api {
-        Segment::fetch_with_privacy(server_address, id, categories, action_types).await
-    } else {
-        Segment::fetch(server_address, id, categories, action_types).await
-    };
+    let mut url = server_address.join("/api/skipSegments").unwrap();
 
-    match segments {
-        Ok(v) => Some(v),
-        Err(e) if e.status() == Some(StatusCode::NOT_FOUND) => None,
-        Err(e) => {
-            log::error!("Failed to get segments: {}", e);
-            None
-        }
-    }
+    url.query_pairs_mut()
+        .append_pair("videoID", &id)
+        .extend_pairs(categories.into_iter().map(|v| ("category", v.to_string())))
+        .extend_pairs(
+            action_types
+                .into_iter()
+                .map(|v| ("actionType", v.to_string())),
+        );
+
+    let req = Client::builder()
+        .user_agent(USER_AGENT)
+        .build()?
+        .get(url)
+        .send()
+        .await?
+        .error_for_status()?;
+
+    Ok(req.json::<Segments>().await?)
+}
+
+pub async fn fetch_with_privacy<C, A>(
+    server_address: Url,
+    id: String,
+    categories: C,
+    action_types: A,
+) -> Result<Segments>
+where
+    C: IntoIterator<Item = Category>,
+    A: IntoIterator<Item = Action>,
+{
+    let mut hasher = Sha256::new(); // create a Sha256 object
+    hasher.update(id); // write input message
+    let hash = hasher.finalize(); // read hash digest and consume hasher
+
+    let mut url = server_address
+        .join("/api/skipSegments/")
+        .unwrap()
+        .join(&hex::encode(hash)[0..4])
+        .unwrap();
+
+    url.query_pairs_mut()
+        .extend_pairs(categories.into_iter().map(|v| ("category", v.to_string())))
+        .extend_pairs(
+            action_types
+                .into_iter()
+                .map(|v| ("actionType", v.to_string())),
+        );
+
+    let req = Client::builder()
+        .user_agent(USER_AGENT)
+        .build()?
+        .get(url)
+        .send()
+        .await?
+        .error_for_status()?;
+
+    Ok(req
+        .json::<Videos>()
+        .await?
+        .into_iter()
+        .find(|v| v.hash == hash.as_slice())
+        .map_or(Segments::default(), |v| v.segments))
 }
--- src/segment.rs
+++ src/segment.rs
@@ -3,11 +3,7 @@ use super::Category;
 
 use std::{cmp, fmt};
 
-use reqwest::{Client, Result, Url};
 use serde_derive::Deserialize;
-use sha2::{Digest, Sha256};
-
-static USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0";
 
 #[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
@@ -30,7 +26,7 @@ pub type Segments = Vec<Segment>;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Video {
+pub struct Video {
     //#[serde(rename = "videoID")]
     //pub video_id: String,
     #[serde(with = "hex")]
@@ -38,76 +34,7 @@ struct Video {
     pub segments: Segments,
 }
 
-type Videos = Vec<Video>;
-
-impl Segment {
-    pub(super) async fn fetch<C, A>(server_address: Url, id: String, categories: C, action_types: A) -> Result<Segments>
-    where
-        C: IntoIterator<Item = Category>,
-        A: IntoIterator<Item = Action>,
-    {
-        let mut url = server_address.join("/api/skipSegments").unwrap();
-
-        url.query_pairs_mut()
-            .append_pair("videoID", &id)
-            .extend_pairs(categories.into_iter().map(|v| ("category", v.to_string())))
-            .extend_pairs(action_types.into_iter().map(|v| ("actionType", v.to_string())));
-
-        let req = Client::builder()
-            .user_agent(USER_AGENT)
-            .build()?
-            .get(url)
-            .send()
-            .await?
-            .error_for_status()?;
-
-        Ok(req.json::<Segments>().await?)
-    }
-
-    pub(super) async fn fetch_with_privacy<C, A>(
-        server_address: Url,
-        id: String,
-        categories: C,
-        action_types: A,
-    ) -> Result<Segments>
-    where
-        C: IntoIterator<Item = Category>,
-        A: IntoIterator<Item = Action>,
-    {
-        let mut hasher = Sha256::new(); // create a Sha256 object
-        hasher.update(id); // write input message
-        let hash = hasher.finalize(); // read hash digest and consume hasher
-
-        let mut url = server_address
-            .join("/api/skipSegments/")
-            .unwrap()
-            .join(&hex::encode(hash)[0..4])
-            .unwrap();
-
-        url.query_pairs_mut()
-            .extend_pairs(categories.into_iter().map(|v| ("category", v.to_string())))
-            .extend_pairs(action_types.into_iter().map(|v| ("actionType", v.to_string())));
-
-        let req = Client::builder()
-            .user_agent(USER_AGENT)
-            .build()?
-            .get(url)
-            .send()
-            .await?
-            .error_for_status()?;
-
-        Ok(req
-            .json::<Videos>()
-            .await?
-            .into_iter()
-            .find(|v| v.hash == hash.as_slice())
-            .map_or(Segments::default(), |v| v.segments))
-    }
-
-    pub fn is_in_segment(&self, time: f64) -> bool {
-        time >= self.segment[0] && time < (self.segment[1] - 0.1_f64)
-    }
-}
+pub type Videos = Vec<Video>;
 
 impl PartialEq for Segment {
     fn eq(&self, other: &Self) -> bool {
@@ -123,6 +50,10 @@ impl PartialOrd for Segment {
 
 impl fmt::Display for Segment {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "[{}] {} - {}", self.category, self.segment[0], self.segment[1])
+        write!(
+            f,
+            "[{}] {} - {}",
+            self.category, self.segment[0], self.segment[1]
+        )
     }
 }
